9289ea9b6af8169048423775ba74c984bdf1e123
GMarcello <917187502@qq.com>
2016-06-14 18:41:49 +0800

    chakslsdlksaklsfk

---
+++ testaaaaaa/testaaaaaa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testaaaaaa
+//
+//  Created by Marcello on 16/6/14.
+//  Copyright © 2016年 Marcello. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
